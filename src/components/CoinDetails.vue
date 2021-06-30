4115e83689a793019716d7e3224351b985fda8f0
Oleksii Biriukov <alexey.birukov@apriorit.com>
2021-06-30 10:52:43 +0300

    add working spinner and not completely working animation when coin added

--- src/components/CoinDetails.vue
+++ src/components/CoinDetails.vue
@@ -1,4 +1,5 @@
 <template>
+  <spinner v-if="isLoading" />
   <div>
     <h1>{{ $route.params.name }}</h1>
     <div>From: {{ this.timeFrom }}</div>
@@ -8,24 +9,34 @@
 </template>
 
 <script>
+import Spinner from "./Spinner.vue";
+
 export default {
   data() {
     return {
       timeFrom: "",
       timeTo: "",
       length: 0,
+      isLoading: true,
     };
   },
 
+  components: {
+    Spinner,
+  },
+
   methods: {
-    async fetchCoinInfo(coin) {
-      let response = await fetch(
-        `https://min-api.cryptocompare.com/data/blockchain/histo/day?fsym=${coin}&api_key=aa9434795b47744b609cbde1f458c1f0b1d0548c273327fd6c5b06209e6e9282`
-      );
-      const data = await response.json();
-      this.timeFrom = data.Data.TimeFrom;
-      this.timeTo = data.Data.TimeTo;
-      this.length = data.Data.Data.length;
+    fetchCoinInfo(coin) {
+      setTimeout(async () => {
+        let response = await fetch(
+          `https://min-api.cryptocompare.com/data/blockchain/histo/day?fsym=${coin}&api_key=aa9434795b47744b609cbde1f458c1f0b1d0548c273327fd6c5b06209e6e9282`
+        );
+        const data = await response.json();
+        this.isLoading = false;
+        this.timeFrom = data.Data.TimeFrom;
+        this.timeTo = data.Data.TimeTo;
+        this.length = data.Data.Data.length;
+      }, 2000);
     },
   },
 
--- src/pages/MainPage.vue
+++ src/pages/MainPage.vue
@@ -1,41 +1,6 @@
 <template>
   <router-view />
   <div class="container mx-auto flex flex-col items-center bg-gray-100 p-4">
-    <!-- <div
-      class="
-        fixed
-        w-100
-        h-100
-        opacity-80
-        bg-purple-800
-        inset-0
-        z-50
-        flex
-        items-center
-        justify-center
-      "
-    >
-      <svg
-        class="animate-spin -ml-1 mr-3 h-12 w-12 text-white"
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-      >
-        <circle
-          class="opacity-25"
-          cx="12"
-          cy="12"
-          r="10"
-          stroke="currentColor"
-          stroke-width="4"
-        ></circle>
-        <path
-          class="opacity-75"
-          fill="currentColor"
-          d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-        ></path>
-      </svg>
-    </div> -->
     <div class="container">
       <add-ticker v-on:add-ticker="add" :addedCoins="tickers">
         <template v-slot:advert>
@@ -129,76 +94,79 @@
         </div>
         <hr class="w-full border-t border-gray-600 my-4" />
         <dl class="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
-          <!-- v-bind:class="sel === t ? 'border-4' : ''"-->
-          <div
-            v-for="t in paginatedTickers"
-            v-bind:key="t.name"
-            v-on:click="select(t)"
-            v-bind:class="{
-              'border-4': selectedTicker === t,
-            }"
-            class="
-              bg-white
-              overflow-hidden
-              shadow
-              rounded-lg
-              border-purple-800 border-solid
-              cursor-pointer
-            "
-          >
-            <div class="px-4 py-5 sm:p-6 text-center">
-              <dt class="text-sm font-medium text-gray-500 truncate">
-                <!-- <router-link v-bind:to="'/coin/'.concat(t.name)">{{
-                  t.name
-                }}</router-link> -->
-                <router-link
-                  :to="{ name: 'coinDetails', params: { name: t.name } }"
-                  >{{ t.name }}</router-link
-                >
-              </dt>
-              <dd class="mt-1 text-3xl font-semibold text-gray-900">
-                {{ t.price }}
-              </dd>
-            </div>
-            <div class="w-full border-t border-gray-200"></div>
-            <div class="text-center">
-              <input type="checkbox" v-model="t.checked" v-on:click.stop="" />
-              <label> Checked</label>
-            </div>
-
-            <button
-              v-on:click.stop="handleDelete(t)"
+          <transition-group name="coins">
+            <!-- v-bind:class="sel === t ? 'border-4' : ''"-->
+            <div
+              v-for="t in paginatedTickers"
+              v-bind:key="t.name"
+              v-on:click="select(t)"
+              v-bind:class="{
+                'border-4': selectedTicker === t,
+              }"
               class="
-                flex
-                items-center
-                justify-center
-                font-medium
-                w-full
-                bg-gray-100
-                px-4
-                py-4
-                sm:px-6
-                text-md text-gray-500
-                hover:text-gray-600 hover:bg-gray-200 hover:opacity-20
-                transition-all
-                focus:outline-none
+                bg-white
+                overflow-hidden
+                shadow
+                rounded-lg
+                border-purple-800 border-solid
+                cursor-pointer
               "
             >
-              <svg
-                class="h-5 w-5"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 20 20"
-                fill="#718096"
-                aria-hidden="true"
+              <div class="px-4 py-5 sm:p-6 text-center">
+                <dt class="text-sm font-medium text-gray-500 truncate">
+                  <!-- <router-link v-bind:to="'/coin/'.concat(t.name)">{{
+                  t.name
+                }}</router-link> -->
+                  <router-link
+                    v-on:click.stop="clearInterval(t.intervalID)"
+                    :to="{ name: 'coinDetails', params: { name: t.name } }"
+                    >{{ t.name }}</router-link
+                  >
+                </dt>
+                <dd class="mt-1 text-3xl font-semibold text-gray-900">
+                  {{ t.price }}
+                </dd>
+              </div>
+              <div class="w-full border-t border-gray-200"></div>
+              <div class="text-center">
+                <input type="checkbox" v-model="t.checked" v-on:click.stop="" />
+                <label> Checked</label>
+              </div>
+
+              <button
+                v-on:click.stop="handleDelete(t)"
+                class="
+                  flex
+                  items-center
+                  justify-center
+                  font-medium
+                  w-full
+                  bg-gray-100
+                  px-4
+                  py-4
+                  sm:px-6
+                  text-md text-gray-500
+                  hover:text-gray-600 hover:bg-gray-200 hover:opacity-20
+                  transition-all
+                  focus:outline-none
+                "
               >
-                <path
-                  fill-rule="evenodd"
-                  d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
-                  clip-rule="evenodd"
-                ></path></svg
-              >Удалить
-            </button>
-          </div>
+                <svg
+                  class="h-5 w-5"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                  fill="#718096"
+                  aria-hidden="true"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
+                    clip-rule="evenodd"
+                  ></path></svg
+                >Удалить
+              </button>
+            </div>
+          </transition-group>
         </dl>
         <hr class="w-full border-t border-gray-600 my-4" />
       </template>
@@ -284,28 +252,32 @@ export default {
   // },
 
   methods: {
+    // eslint-disable-next-line no-unused-vars
     subscribeForUpdates(tickerName) {
+      // eslint-disable-next-line no-unused-vars
       let intervalID = setInterval(async () => {
-        let response = await fetch(
-          `https://min-api.cryptocompare.com/data/price?fsym=${tickerName}&tsyms=USD&api_key=aa9434795b47744b609cbde1f458c1f0b1d0548c273327fd6c5b06209e6e9282`
-        );
-        const data = await response.json();
-        let t = this.tickers.find((t) => t.name === tickerName);
-        t.price = data.USD;
-        t.intervalID = intervalID;
-
-        if (this.selectedTicker?.name === tickerName) {
-          this.$refs.graphComponent.graph.push(data.USD);
-        }
+        // let response = await fetch(
+        //   `https://min-api.cryptocompare.com/data/price?fsym=${tickerName}&tsyms=USD&api_key=aa9434795b47744b609cbde1f458c1f0b1d0548c273327fd6c5b06209e6e9282`
+        // );
+        // const data = await response.json();
+        // let t = this.tickers.find((t) => t.name === tickerName);
+        // t.price = data.USD;
+        // t.intervalID = intervalID;
+        // if (this.selectedTicker?.name === tickerName) {
+        //   this.$refs.graphComponent.graph.push(data.USD);
+        // }
       }, 3000);
     },
-
+    randomIndex: function () {
+      return Math.floor(Math.random() * this.tickers.length);
+    },
     add(newName = this.ticker) {
       newName = newName.toUpperCase();
       let newTicker = { name: newName, price: "-" };
       if (this.tickers.find((t) => t.name === newName) == undefined) {
         this.isTickerExists = false;
-        this.tickers.push(newTicker);
+        this.tickers.splice(this.randomIndex(), 0, newTicker);
+        //this.tickers.push(newTicker);
         this.subscribeForUpdates(newTicker.name);
 
         localStorage.setItem("watched-coins", JSON.stringify(this.tickers));
@@ -350,3 +322,4 @@ export default {
 </script>
 
 <style src="../app.css"></style>
+<style src="../animation.css"></style>
